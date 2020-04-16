7816dc806e3cbccb99e25a8155bf4387ded20ed1
Ян <gorshkov.yan1998@gmail.com>
2020-04-17 00:27:20 +0300

    Implemented RememberME

--- client/views.py
+++ client/views.py
@@ -1,12 +1,15 @@
-
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
 from client.models import Client
+import datetime
+from django.conf import settings
+from django.template import RequestContext, loader
 
 
 # Create your views here.
 def auth_index(request):
+    if hasattr(request.COOKIES, 'customerId'):
+        request.session['client'] = str(request.COOKIES['customerId'])
     return render(request, "client/authindex.html", {})
 
 
@@ -14,10 +17,12 @@ def auth_main(request):
     if request.POST:
         tmp_cl = Client.objects.filter(email=request.POST['email'], password=request.POST['password']).values('id',
                                                                                                               'email').first()
-        print(tmp_cl)
         if tmp_cl:
             request.session['client'] = str(tmp_cl['email']) + "---" + str(tmp_cl['id'])
-            return redirect('/cabinet')
+            response = HttpResponse(render(request, 'client/cabinet.html', {}))
+            if request.POST.get('remember-me'):
+                response.set_cookie("customerId", tmp_cl['id'])
+            return response
         else:
             return redirect('/')
     else:
@@ -32,8 +37,10 @@ def cabinet(request):
 
 
 def logout(request):
+    response = HttpResponseRedirect("/")
+    if request.COOKIES.get('customerId'):
+        response.delete_cookie('customerId')
+        
     if request.session.get('client'):
         del request.session['client']
-        return redirect('/')
-    else:
-        return redirect('/cabinet')
+    return response
